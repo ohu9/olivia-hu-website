74936aecd1333b455b281bb40a2f5070770eb96b
ohu9 <oliviah221@gmail.com>
2024-08-20 15:17:11 -0400

    new build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
 		extend: {
@@ -33,3 +31,4 @@ const config: Config = {
 };
 
 export default config;
+
